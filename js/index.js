471027188394dc19e2e7f85d295e0d0150a13102
jaehyanggg <jaehyanggg@gmail.com>
2021-05-07 11:11:44 +0900

    bg

--- js/index.js
+++ js/index.js
@@ -15,15 +15,13 @@ $(function() {
 		i50d: 'bi-cloud-haze',
 	}
 
-	var $stageWrapper = $('.stage-wrapper');
+	var $bgWrapper = $('.bg-wrapper');
 
 
 
 	/*************** 사용자 함수 *****************/
-	init();
-	function init() {
-		initBg();
-	}
+	initBg();
+	
 
 	function initBg() {
 		var d = new Date();
@@ -35,8 +33,8 @@ $(function() {
 		(time >= 14 && time < 18) ? 4 :
 		(time >= 18 && time < 22) ? 5 : 6;
 
-		for(var i=1; i<=6; i++) $stageWrapper.removeClass('active'+i);
-		$stageWrapper.addClass('active'+timeDivision);
+		for(var i=1; i<=6; i++) $bgWrapper.removeClass('active'+i);
+		$bgWrapper.addClass('active'+timeDivision);
 	}
 
 
